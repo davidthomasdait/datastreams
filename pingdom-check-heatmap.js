6235e5f8ac18bddbb05e68e93d6f04a6566cf409
David Thomas <>
2021-05-22 15:58:04 +0000

    transform data

--- pingdom-check-heatmap.js
+++ pingdom-check-heatmap.js
@@ -1,26 +1,35 @@
 async function run(config, context, timeframe, fetcher) {
-        let data;
+    let data;
 
-        if (context.sourceId && context.sourceName) {
-                const sourceName = context.sourceName[0];
-                if (sourceName !== 'Pingdom') {
-                        return 'Object is not a Pingdom check';
-                }
-                data = [context];
-        } else {
-                const limit = (config.vars && config.vars.limit) || 10;
-                const gremlinQuery = 'g.V().has("sourceName", sourceName).limit(limit).valueMap(true)';
-                const bindings = { sourceName: 'Pingdom', limit };
-                const graphConfig = { gremlinQuery, bindings };
-                data = await fetcher('graph-custom', graphConfig, context, timeframe);
+    if (context.sourceId && context.sourceName) {
+        const sourceName = context.sourceName[0];
+        if (sourceName !== 'Pingdom') {
+                return 'Object is not a Pingdom check';
         }
+        data = [context];
+    } else {
+        const limit = (config.vars && config.vars.limit) || 10;
+        const gremlinQuery = 'g.V().has("sourceName", sourceName).limit(limit).valueMap(true)';
+        const bindings = { sourceName: 'Pingdom', limit };
+        const graphConfig = { gremlinQuery, bindings };
+        data = await fetcher('graph-custom', graphConfig, context, timeframe);
+    }
 
-        return Promise.all(data.map(async ctx => {
-                const pingdomConfig = {
-                        id: ctx.sourceId[0]
-                        //includeUpTime: true,
-                        //resultProp: 'downtime'
-                }
-                return fetcher('pingdom', pingdomConfig, ctx, timeframe);
-        }));
+    data = await Promise.all(data.map(async ctx => {
+        const pingdomConfig = {
+                id: ctx.sourceId[0]
+        }
+        return fetcher('pingdom', pingdomConfig, ctx, timeframe);
+    }));
+
+    return data.reduce((final, obj) => {
+        const check = obj.series[0];
+        const name = check.id.split(' Response Time ')[0];
+        const props = check.data.reduce((row, datum) => {
+            row[`${datum.timestamp}`] = datum.value;
+            return row;
+        }, {});
+        final.push( { name, ...props } );
+        return final;
+    }, []);
 }
