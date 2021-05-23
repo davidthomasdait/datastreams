d0e74c51e40ed517442da92b507ebaff1466c948
David Thomas <>
2021-05-23 11:35:29 +0000

    log timeframe enum

--- pingdom-check-heatmap.js
+++ pingdom-check-heatmap.js
@@ -14,7 +14,7 @@ async function getGraphNodes(config, context, timeframe, fetcher) {
 }
 
 async function run(config, context, timeframe, fetcher) {
-    console.log('timeframe:', typeof timeframe, timeframe);
+    console.log('timeframe.enum:', typeof timeframe.enum, timeframe);
 
     /* Determine the context (Pingdom graph nodes) for the Pingdom data request */
     const nodes = await getGraphNodes(config, context, timeframe, fetcher);
