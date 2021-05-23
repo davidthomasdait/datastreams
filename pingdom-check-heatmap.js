e795fe921aab006f4d2d5edee95f9bf181e1f014
davidthomasdait <84625124+davidthomasdait@users.noreply.github.com>
2021-05-23 19:05:20 +0100

    Update pingdom-check-heatmap.js

--- pingdom-check-heatmap.js
+++ pingdom-check-heatmap.js
@@ -14,30 +14,35 @@ async function getGraphNodes(config, context, timeframe, fetcher) {
 }
 
 async function run(config, context, timeframe, fetcher) {
-    /* Determine the context (Pingdom graph nodes) for the Pingdom data request */
-    const nodes = await getGraphNodes(config, context, timeframe, fetcher);
+    try {
+        /* Determine the context (Pingdom graph nodes) for the Pingdom data request */
+        const nodes = await getGraphNodes(config, context, timeframe, fetcher);
 
-    /* Get the Pingdom timeseries data for the Pingdom graph nodes */
-    const data = await Promise.all(nodes.map(async node => {
-        const pingdomConfig = {
-                id: node.sourceId[0]
-        }
-        return fetcher('pingdom', pingdomConfig, node, timeframe);
-    }));
+        /* Get the Pingdom timeseries data for the Pingdom graph nodes */
+        const data = await Promise.all(nodes.map(async node => {
+            const pingdomConfig = {
+                    id: node.sourceId[0]
+            }
+            return fetcher('pingdom', pingdomConfig, node, timeframe);
+        }));
 
-    /* Transform the Pingdom timeseries data to that required by a Heatmap */
-    return data.reduce((final, obj) => {
-        const check = obj.series[0];
-        const name = check.id.split(' Response Time ')[0];
-        const props = check.data.reduce((row, datum) => {
-            const options = timeframe.includes('day') ?
-                { day: 'numeric', month: 'short' } :
-                { hour: 'numeric', minute: 'numeric'};
-            const key = new Date(datum.timestamp).toLocaleString('en-GB', options);
-            row[key] = datum.value;
-            return row;
-        }, {});
-        final.push( { name, ...props } );
-        return final;
-    }, []);
+        /* Transform the Pingdom timeseries data to that required by a Heatmap */
+        return data.reduce((final, obj) => {
+            const check = obj.series[0];
+            const name = check.id.split(' Response Time ')[0];
+            const props = check.data.reduce((row, datum) => {
+                const options = timeframe.enum.includes('day') ?
+                    { day: 'numeric', month: 'short' } :
+                    { hour: 'numeric', minute: 'numeric'};
+                const key = new Date(datum.timestamp).toLocaleString('en-GB', options);
+                row[key] = datum.value;
+                return row;
+            }, {});
+            final.push( { name, ...props } );
+            return final;
+        }, []);
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
 }
